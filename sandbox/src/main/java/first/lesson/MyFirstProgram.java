fd15e87d3a94b95f24634dbf7a082243b8fa31cf
Sergey Khristenko <sergey.khristenko@gmail.com>
2017-02-11 23:12:36 +0200

    add class Point, change main method to run the Point.distance calculation

--- sandbox/src/main/java/first/lesson/MyFirstProgram.java
+++ sandbox/src/main/java/first/lesson/MyFirstProgram.java
@@ -1,8 +1,15 @@
 package first.lesson;
 
+
 public class MyFirstProgram {
 
-  public static void main(String[] args){
-	System.out.println("Hello, World!");
+  public static void main(String[] args) {
+
+    Point p1 = new Point(-4, 0);
+    Point p2 = new Point(100, 100);
+
+    System.out.println(Point.distance(p1, p2));
+
   }
+
 }
---
+++ sandbox/src/main/java/first/lesson/Point.java
@@ -0,0 +1,17 @@
+package first.lesson;
+
+public class Point {
+
+  private double x, y;
+
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+
+  }
+}
