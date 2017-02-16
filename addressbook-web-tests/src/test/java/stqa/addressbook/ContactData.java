2cd248958223d3a3ddfc2462b79cbca0acc48f66
Sergey Khristenko <sergey.khristenko@gmail.com>
2017-02-16 23:20:09 +0200

    add test for contact creation

---
+++ addressbook-web-tests/src/test/java/stqa/addressbook/ContactData.java
@@ -0,0 +1,67 @@
+package stqa.addressbook;
+
+public class ContactData {
+  private final String name;
+  private final String middleName;
+  private final String lastName;
+  private final String nickname;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String homePhone;
+  private final String mobile;
+  private final String email;
+
+  public ContactData(String name, String middleName, String lastName, String nickname, String title, String company, String address, String homePhone, String mobile, String email) {
+    this.name = name;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.homePhone = homePhone;
+    this.mobile = mobile;
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomePhone() {
+    return homePhone;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
