15e687aa10a5eb2ef4ce733e9957801a0f462208
Sergey Khristenko <sergey.khristenko@gmail.com>
2017-04-09 23:17:43 +0300

    l6 m10 - create config file with environment preferences (baseUrl, login/pass)

--- addressbook-web-tests/src/test/java/stqa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/tests/TestBase.java
@@ -8,7 +8,8 @@ import stqa.addressbook.appManager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app =
+          new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeTest
   public void setUp() throws Exception {
