533e166fdd0845ef135d2dc674af28cd713e7351
Sergey Khristenko <sergey.khristenko@gmail.com>
2017-04-16 01:49:58 +0300

    l8m6 - add smtp server

--- mantis-tests/src/test/java/stqa/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/stqa/mantis/appmanager/RegistrationHelper.java
@@ -1,6 +1,8 @@
 package stqa.mantis.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import stqa.mantis.tests.TestBase;
 
 public class RegistrationHelper {
   private final ApplicationManager app;
@@ -13,5 +15,16 @@ public class RegistrationHelper {
 
   public void start(String username, String email) {
     wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+    wd.findElement(By.name("username")).sendKeys(username);
+    wd.findElement(By.name("email")).sendKeys(email);
+    wd.findElement(By.cssSelector("input[type='submit']")).click();
+
+  }
+
+  public void finish(String confirmationLink, String password) {
+    wd.get(confirmationLink);
+    wd.findElement(By.name("password")).sendKeys(password);
+    wd.findElement(By.name("password_confirm")).sendKeys(password);
+    wd.findElement(By.cssSelector("button[type='submit']")).click();
   }
 }
--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -9,3 +9,7 @@ $g_default_timezone       = 'Europe/Berlin';
 
 $g_crypto_master_salt     = 'nLgQp1V82dLPXhjaIZkDMyY7FcFthysKf1xS2NEZl5U=';
 $g_signup_use_captcha     = OFF;
+
+$g_phpMailer_method       = PHPMAILER_METHOD_SMTP;
+$g_smtp_host              = 'localhost';
+$g_smtp_port = 4200;
