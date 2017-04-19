544a0b223e2d6e454ca0aded5e752ed5393c2dcf
Sergey Khristenko <sergey.khristenko@gmail.com>
2017-04-19 23:43:56 +0300

    add remote web server capabilities

--- addressbook-web-tests/src/test/java/stqa/addressbook/appManager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/appManager/ApplicationManager.java
@@ -1,15 +1,18 @@
 package stqa.addressbook.appManager;
 
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -37,10 +40,16 @@ public class ApplicationManager {
 
     dbHelper = new DbHelper();
 
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
+    if ("".equals(properties.getProperty("selenium.server"))) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      }
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
 
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
@@ -52,7 +61,6 @@ public class ApplicationManager {
     contactHelper = new ContactHelper(wd);
 
     sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
-    wd.manage().window().setSize(new Dimension(1024,768));
   }
 
 
